f5737abb20b7893a15aa26bc9f3c9e476152ea2d
Imod7 <dsaripapazoglou@gmail.com>
2023-02-02 17:55:15 +0100

    Added calls to query endpoints by name & results of queries - Added the calls to query all the endpoints also by pallet name (apart from the pallet index). - Added the results from running the script to the test-results folder in the corresponding files. - Refactored the code to avoid code duplication (DRY principle).

--- main.js
+++ main.js
@@ -15,43 +15,50 @@ const getData = async (endpoint) => {
   try {
     const response = await axios.get(`${BASE_URL}` + endpoint);
     console.log(`${colours.fg.green} Success: ${response.status} ${colours.reset}`);
+    console.log(`${BASE_URL}${endpoint}`);
     return response;
   } catch (errors) {
     console.log(`${colours.fg.red} Error: ${errors.response.status} ${colours.reset}`);
+    console.log(`${BASE_URL}${endpoint}`);
     return errors;
   }
 };
 
+const queryEndpoint = async (pallets, param, endpoint) => {
+  console.log(`${colours.fg.cyan}\nTesting ${endpoint} Endpoint for All Pallets by pallet ${param}`);
+  console.log(`==================================================================== ${colours.reset}`);
+  for (var i=0; i < pallets.length; i++) {
+    process.stdout.write(`Pallet Index: ${colours.fg.cyan}` + pallets[i]['index'] + `${colours.reset} - Pallet Name: ${colours.fg.cyan}` + pallets[i]['name'] + `${colours.reset}\t ->  `);
+    const errors = await getData('/pallets/' + pallets[i][param] + `/${endpoint} \n`);
+  }
+}
+
 const main = async () => {
-  console.log(`${colours.fg.cyan}Retrieving Metadata + Pallets Indices`)
-  console.log(`===================================== ${colours.reset}`)
-  const metadata = await getData('/runtime/metadata')
+  console.log(`${colours.fg.cyan}Retrieving Metadata + Pallets Indices`);
+  console.log(`===================================== ${colours.reset}`);
+  const metadata = await getData('/runtime/metadata');
   const pallets = metadata.data.metadata.v14.pallets;
 
-  // Querying the STORAGE Endpoint for All Pallets
-  console.log(`${colours.fg.cyan}\nTesting Storage Endpoint for All Pallets`)
-  console.log(`===================================== ${colours.reset}`)
-  for (var i=0; i < pallets.length; i++) {
-    process.stdout.write('pallet: ' + pallets[i]['index'] + " - " + pallets[i]['name'] + "\t\t ->  ")
-    const errors = await getData('/pallets/' + pallets[i]['index'] + '/storage');
-  }
+  // STORAGE endpoint
+  // Querying the storage endpoint for All Pallets by pallet INDEX
+  const storageByIndex = await queryEndpoint(pallets, 'index', 'storage');
 
-  // Querying the ERRORS Endpoint for All Pallets
-  console.log(`${colours.fg.cyan}\nTesting Errors Endpoint for All Pallets`)
-  console.log(`===================================== ${colours.reset}`)
-  for (var i=0; i < pallets.length; i++) {
-    process.stdout.write('pallet: ' + pallets[i]['index'] + " - " + pallets[i]['name'] + "\t\t ->  ")
-    const errors = await getData('/pallets/' + pallets[i]['index'] + '/errors')
-  }
+  // Querying the storage endpoint for All Pallets by pallet NAME
+  const storageByName = await queryEndpoint(pallets, 'name', 'storage');
 
-  // Querying the EVENTS Endpoint for All Pallets
-  console.log(`${colours.fg.cyan}\nTesting Events Endpoint for All Pallets`)
-  console.log(`===================================== ${colours.reset}`)
-  for (var i=0; i < pallets.length; i++) {
-    process.stdout.write('pallet: ' + pallets[i]['index'] + " - " + pallets[i]['name'] + "\t\t ->  ")
-    const errors = await getData('/pallets/' + pallets[i]['index'] + '/events')
-  }
+  // ERRORS endpoint
+  // Querying the ERRORS endpoint for All Pallets by pallet INDEX
+  const errorsByIndex = await queryEndpoint(pallets, 'index', 'errors');
 
+  // Querying the EVENTS endpoint for All Pallets by pallet NAME
+  const errorsByName = await queryEndpoint(pallets, 'name', 'errors');
+
+  // EVENTS endpoint
+  // Querying the events endpoint for All Pallets by pallet INDEX
+  const eventsByIndex = await queryEndpoint(pallets, 'index', 'events');
+
+  // Querying the events endpoint for All Pallets by pallet NAME
+  const eventsByName = await queryEndpoint(pallets, 'name', 'events');
 };
 
 main();
