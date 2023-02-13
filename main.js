e258592dd27f0526ae79b7312f2a8b457788d01c
Imod7 <dsaripapazoglou@gmail.com>
2023-02-13 15:41:28 +0100

    Making easier the comparison of failed requests - Added in an array all failed requests & sorted it. - Counted the occurences of each endpoint in the failed requests array. - Added the `consts` endpoint in the requests.

--- main.js
+++ main.js
@@ -11,54 +11,61 @@ const colours = {
 
 const BASE_URL = 'http://127.0.0.1:8080';
 
+const failedRequests = [];
+
 const getData = async (endpoint) => {
   try {
     const response = await axios.get(`${BASE_URL}` + endpoint);
     console.log(`${colours.fg.green} SUCCESS: ${response.status} ${colours.reset}`);
-    console.log(`${BASE_URL}${endpoint}`);
+    // console.log(`${BASE_URL}${endpoint}`);
     return response;
   } catch (errors) {
     console.log(`${colours.fg.red} FAIL: ${errors.response.status} ${colours.reset}`);
-    console.log(`${BASE_URL}${endpoint}`);
+    // console.log(`${BASE_URL}${endpoint}`);
     return errors;
   }
 };
 
 const queryEndpoint = async (pallets, param, endpoint) => {
-  console.log(`${colours.fg.cyan}\nTesting ${endpoint} Endpoint for All Pallets by pallet ${param}`);
+  console.log(`\nTesting ${colours.fg.green} ${endpoint} ${colours.reset} Endpoint for All Pallets by pallet ${colours.fg.green} ${param} ${colours.reset}`);
   console.log(`==================================================================== ${colours.reset}`);
   for (var i=0; i < pallets.length; i++) {
     process.stdout.write(`Pallet Index: ${colours.fg.cyan}` + pallets[i]['index'] + `${colours.reset} - Pallet Name: ${colours.fg.cyan}` + pallets[i]['name'] + `${colours.reset}\t ->  `);
-    const errors = await getData('/pallets/' + pallets[i][param] + `/${endpoint} \n`);
+    const result = await getData('/pallets/' + pallets[i][param] + `/${endpoint} \n`);
+    // console.log(result.status)
+    if (result.status != '200') {
+      failedRequests.push([parseInt(pallets[i]['index']), pallets[i]['name'], endpoint, pallets[i][param], result.status]);
+    }
   }
 }
 
 const main = async () => {
-  console.log(`${colours.fg.cyan}Retrieving Metadata + Pallets Indices`);
-  console.log(`===================================== ${colours.reset}`);
+  console.log(`Retrieving Metadata + Pallets Indices`);
+  console.log(`===================================== `);
   const metadata = await getData('/runtime/metadata');
   const pallets = metadata.data.metadata.v14.pallets;
 
-  // STORAGE endpoint
-  // Querying the storage endpoint for All Pallets by pallet INDEX
   const storageByIndex = await queryEndpoint(pallets, 'index', 'storage');
-
-  // Querying the storage endpoint for All Pallets by pallet NAME
   const storageByName = await queryEndpoint(pallets, 'name', 'storage');
-
-  // ERRORS endpoint
-  // Querying the ERRORS endpoint for All Pallets by pallet INDEX
   const errorsByIndex = await queryEndpoint(pallets, 'index', 'errors');
-
-  // Querying the EVENTS endpoint for All Pallets by pallet NAME
   const errorsByName = await queryEndpoint(pallets, 'name', 'errors');
-
-  // EVENTS endpoint
-  // Querying the events endpoint for All Pallets by pallet INDEX
   const eventsByIndex = await queryEndpoint(pallets, 'index', 'events');
-
-  // Querying the events endpoint for All Pallets by pallet NAME
   const eventsByName = await queryEndpoint(pallets, 'name', 'events');
+  const constsByIndex = await queryEndpoint(pallets, 'index', 'consts');
+  const constsByName = await queryEndpoint(pallets, 'name', 'consts');
+
+  failedRequests.sort();
+  console.log(`Array with All Failed Requests`);
+  console.log(`============================== `);
+  console.log(failedRequests);
+
+  const occurrences = failedRequests.reduce(function (acc, curr) {
+    return acc[curr[2]] ? ++acc[curr[2]] : acc[curr[2]] = 1, acc
+  }, {});
+  
+  console.log(`Counting the Failed Requests per Endpoint (should be even numbers)`);
+  console.log(`================================================================= `);
+  console.log("Occurences : ", occurrences) 
 };
 
 main();
